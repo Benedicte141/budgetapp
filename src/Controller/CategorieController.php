fc23b63d2a37c7889d7c7eca4499a850fec4d791
benedicte.gady <benedicte.gady@sts-sio-caen.info>
2024-12-06 16:57:31 +0100

    Création Vue Catégorie

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Categorie;
 
 class CategorieController extends AbstractController
 {
-    #[Route('/categorie', name: 'app_categorie')]
-    public function index(): Response
-    {
-        return $this->render('categorie/index.html.twig', [
-            'controller_name' => 'CategorieController',
-        ]);
+    #[Route('/categorie/list', name: 'app_categorie_list')]
+    public function listerCategories(EntityManagerInterface $entityManager){
+        //$categories= $doctrine->getRepository(Categorie::class)->findAll();
+        $categories = $entityManager->getRepository(Categorie::class)->findAll();
+
+        return $this->render('categorie/lister.html.twig', [
+            'categories' => $categories,]);	
+
     }
 }

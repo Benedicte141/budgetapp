ac43db2c371cb36a3e5ea599c63387430af47d9a
benedicte.gady <benedicte.gady@sts-sio-caen.info>
2024-12-17 11:55:32 +0100

    Création EmpruntController

---
+++ src/Controller/EmpruntController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class EmpruntController extends AbstractController
+{
+    #[Route('/emprunt', name: 'app_emprunt')]
+    public function index(): Response
+    {
+        return $this->render('emprunt/index.html.twig', [
+            'controller_name' => 'EmpruntController',
+        ]);
+    }
+}
+

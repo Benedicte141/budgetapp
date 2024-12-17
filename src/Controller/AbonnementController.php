55f8dbd6b050593d93ec823061cf2eca2d5fc27f
Skrylleur <106743803+Skrylleur@users.noreply.github.com>
2024-12-17 11:31:10 +0100

    Création du controller AbonnementController.php

---
+++ src/Controller/AbonnementController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Abonnement;
+
+class CompteController extends AbstractController
+{
+    #[Route('/abonnement', name: 'app_abonnement_liste')]
+
+    public function listerAbonnements(EntityManagerInterface $entityManager){
+        $abonnements = $entityManager->getRepository(Abonnement::class)->findAll();
+
+        return $this->render('abonnement/lister.html.twig', [
+            'abonnements' => $abonnements,]);
+    }
+}

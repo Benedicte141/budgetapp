4bf6e9435e137000198b756152e1c0654d8d1d19
benedicte.gady <benedicte.gady@sts-sio-caen.info>
2024-12-19 13:31:29 +0100

    Vue Emprunt créée

--- src/Controller/EmpruntController.php
+++ src/Controller/EmpruntController.php
@@ -5,15 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Emprunt;
 
 class EmpruntController extends AbstractController
 {
-    #[Route('/emprunt', name: 'app_emprunt')]
-    public function index(): Response
-    {
-        return $this->render('emprunt/index.html.twig', [
-            'controller_name' => 'EmpruntController',
-        ]);
-    }
+    #[Route('/emprunt/list', name: 'app_emprunt_list')]
+    
+    public function listerEmprunts(EntityManagerInterface $entityManager){
+		//$emprunts= $doctrine->getRepository(Emprunt::class)->findAll();
+        $emprunts = $entityManager->getRepository(Emprunt::class)->findAll();
+
+		return $this->render('emprunt/lister.html.twig', [
+            'emprunts' => $emprunts,]);	
+ 
+	}
 }
+
+
 

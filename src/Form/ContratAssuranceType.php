d055476259c750dabf6e82cd290ec1b1778792aa
unknown <anaelle.bargas@sts-sio-caen.info>
2024-12-19 16:44:26 +0100

    css tables et formulaire creation contrats assurance vie

---
+++ src/Form/ContratAssuranceType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CompagnieAssurance;
+use App\Entity\ContratAssuranceVie;
+use App\Entity\ModeGestion;
+use App\Entity\Periodicite;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContratAssuranceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('solde')
+            ->add('montantValueLatente')
+            ->add('totalInvestit')
+            ->add('numero')
+            ->add('totalRachete')
+            ->add('dateOuverture', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('montantVersementLibre')
+            ->add('nom')
+            ->add('compagnie_assurance', EntityType::class, [
+                'class' => CompagnieAssurance::class,
+'choice_label' => 'nom',
+            ])
+            ->add('mode_gestion', EntityType::class, [
+                'class' => ModeGestion::class,
+'choice_label' => 'libelle',
+            ])
+            ->add('periodicite', EntityType::class, [
+                'class' => Periodicite::class,
+'choice_label' => 'libelle',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ContratAssuranceVie::class,
+        ]);
+    }
+}

6bf30e1c81ca6f84db50368d440b6b96e27d35ce
benedicte.gady <benedicte.gady@sts-sio-caen.info>
2024-12-20 18:56:17 +0100

    Soumission des données Ok

--- src/Controller/EmpruntController.php
+++ src/Controller/EmpruntController.php
@@ -11,23 +11,33 @@ use App\Form\EmpruntType;
 use Symfony\Component\HttpFoundation\Request;
 
 
-
 class EmpruntController extends AbstractController
 {
-    #[Route('/emprunt/list', name: 'app_emprunt_list')]
+    #[Route('/emprunt/lister', name: 'app_emprunt_list')]
     
     public function listerEmprunts(EntityManagerInterface $entityManager){
 		//$emprunts= $doctrine->getRepository(Emprunt::class)->findAll();
         $emprunts = $entityManager->getRepository(Emprunt::class)->findAll();
-        return $this->render('emprunt/list.html.twig', ['emprunts' => $emprunts]);
+        return $this->render('emprunt/lister.html.twig', ['emprunts' => $emprunts]);
     }
     
     #[Route('/emprunt/ajouter', name: 'app_emprunt_ajouter')]  
-    public function ajouterEmprunt(){
+    public function ajouterEmprunt(Request $request, EntityManagerInterface $entityManager){
  
 		$emprunt = new emprunt();
-		$form = $this->createForm(EmpruntType::class, $emprunt);
-                return $this->render('emprunt/ajouter.html.twig', array(
-                'form' => $form->createView(), ));
+        $form = $this->createForm(EmpruntType::class, $emprunt);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+ 
+            $entityManager->persist($emprunt);
+            $entityManager->flush();
+
+   	        return $this->render('emprunt/ajouter.html.twig', ['emprunt' => $emprunt,]);
+        }
+        else
+        {
+            return $this->render('emprunt/ajouter.html.twig', array('form' => $form->createView(), ));
+        }
     }
 }

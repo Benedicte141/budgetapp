123647bf73fa269104f70019dabf73f94122f1c8
unknown <anaelle.bargas@sts-sio-caen.info>
2024-12-23 12:59:30 +0100

    nav-bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('daisyui'), // Ajouter DaisyUI comme plugin
+    require('daisyui', require('tailwind-scrollbar')), // Ajouter DaisyUI comme plugin
   ],
 }
 
